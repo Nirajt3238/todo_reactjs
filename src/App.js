07725b0087b4def0f183ae65c52530a0c10bad31
Niraj Tiwari <86589597+Nirajt3238@users.noreply.github.com>
2022-07-08 16:56:05 +0530

    todo project in react js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn to in react js Project
         </a>
       </header>
     </div>
