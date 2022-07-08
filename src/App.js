537dbaee2d2813fa6c75c54f200f1a377b8e4348
Niraj Tiwari <86589597+Nirajt3238@users.noreply.github.com>
2022-07-08 17:21:57 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn to in react js
+          Learn to in react js project
         </a>
       </header>
     </div>
